418ec940843a9b8749b2c6ab9620b1fa5b5fb0f3
Haluk Alireisoğlu <hlkalireisoglu61@gmail.com>
2024-07-07 17:46:06 +0300

    Add files via upload

---
+++ Java102/Course8 - MultiThreads and Concurrency/ThreadsRace/JavaKurs.java
@@ -0,0 +1,30 @@
+package JavaThreadKurs;
+
+import java.util.*;
+
+public class JavaKurs {
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Thread> threads = new ArrayList<>();
+        ThreadHomework threadProcess = new ThreadHomework();
+
+        for (int i = 0; i <= 3; i++) {
+            Thread t = new Thread(threadProcess);
+            threads.add(t);
+        }
+
+        threads.get(0).start();
+        threads.get(1).start();
+        threads.get(2).start();
+        threads.get(3).start();
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("Time left for writing the lists: " + (5 - i));
+            Thread.sleep(1000L);
+        }
+        System.out.println();
+
+        System.out.println("Even List Length: " + threadProcess.evenNumbers.size() + " / " + "Even numbers: " + threadProcess.evenNumbers);
+        System.out.println("Odd List Length: " + threadProcess.oddNumbers.size() + " / " + "Odd numbers: " + threadProcess.oddNumbers);
+    }
+}
---
+++ Java102/Course8 - MultiThreads and Concurrency/ThreadsRace/ThreadHomework.java
@@ -0,0 +1,39 @@
+package JavaThreadKurs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadHomework implements Runnable {
+
+    public ArrayList<Integer> evenNumbers = new ArrayList<>();
+    public ArrayList<Integer> oddNumbers = new ArrayList<>();
+
+    static List<ArrayList> arrayLists = new ArrayList<>();
+
+    public ThreadHomework() {
+        for (int i = 0; i <= 3; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            arrayLists.add(list);
+            for (int j = 1; j <= 2500; j++) {
+                arrayLists.get(i).add(j + (i * 2500));
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        byte[] threadName = Thread.currentThread().getName().getBytes();
+        int x = threadName[7] - 48;
+        numberFinder(x);
+    }
+
+    public synchronized void numberFinder(int x) {
+        for (int i = 0; i < 2500; i++) {
+            if ((Integer) arrayLists.get(x).get(i) % 2 == 0) {
+                evenNumbers.add((Integer) arrayLists.get(x).get(i));
+            } else {
+                oddNumbers.add((Integer) arrayLists.get(x).get(i));
+            }
+        }
+    }
+}
